f536b72fb2170485b957ac8a736143ee32af9134
Andrews, John L <John.L.Andrews@disney.com>
2016-07-19 19:47:57 -0700

    add networking code

--- app/src/main/java/codepath/movies/MovieActivity.java
+++ app/src/main/java/codepath/movies/MovieActivity.java
@@ -1,13 +1,54 @@
 package codepath.movies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Movie;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MovieActivity extends AppCompatActivity {
+
+    private  static final String baseMovieURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    ArrayList<Movie> movies;
+    MovieArrayAdapter movieAdapter;
+    ListView lvItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+    }
+
+    private void networkingCodeSetup() {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(baseMovieURL, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray movieJSONresults = response.getJSONArray("results");
+                    movies = Movie.fromJSONArray(movieJSONresults);
+                    movieAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
     }
 }
