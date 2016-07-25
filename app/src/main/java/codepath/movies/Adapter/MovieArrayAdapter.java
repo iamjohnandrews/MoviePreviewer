2d6547f0de7a1de3975d48476641eddae91c82d6
Andrews, John L <John.L.Andrews@disney.com>
2016-07-24 19:36:01 -0700

    populate title and overview in custom array adapter

--- app/src/main/java/codepath/movies/Adapter/MovieArrayAdapter.java
+++ app/src/main/java/codepath/movies/Adapter/MovieArrayAdapter.java
@@ -1,10 +1,17 @@
 package codepath.movies.Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.List;
 
 import codepath.movies.Model.Movie;
+import codepath.movies.R;
 
 /**
  * Created by andrj148 on 7/19/16.
@@ -15,5 +22,26 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
     public MovieArrayAdapter(Context context, List<Movie> movies) {
         super(context, android.R.layout.simple_list_item_1, movies);
 
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        if (convertView == null) {
+            LayoutInflater inflator = LayoutInflater.from(getContext());
+            convertView = inflator.inflate(R.layout.item_movie, parent, false);
+        }
+
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+
+        return convertView;
     }
 }
